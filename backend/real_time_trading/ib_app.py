9e8840cfa67bd9f209c51ef2dd2f98f2d41d1261
Hongru Liu <hl3148@columbia.edu>
2024-02-06 01:34:38 -0500

    fix root level logger

--- backend/real_time_trading/ib_app.py
+++ backend/real_time_trading/ib_app.py
@@ -23,8 +23,8 @@ formatter = logging.Formatter(
     "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
